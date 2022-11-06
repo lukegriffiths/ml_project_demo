fc9dec63d119801c9b01249138692760044b97ad
Luke Griffiths <griff.luke@gmail.com>
2022-11-06 16:58:30 +0100

    Updated run script with timer

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-python train.py --fold 0 --model decision_tree_entropy
-python train.py --fold 1 --model decision_tree_gini
-python train.py --fold 2 --model decision_tree_entropy
-python train.py --fold 3 --model decision_tree_entropy
-python train.py --fold 4  --model decision_tree_entropy      
+# current time 
+START="$(date +%s)"
+
+python train.py --fold 0 --model rf
+python train.py --fold 1 --model rf
+python train.py --fold 2 --model rf
+python train.py --fold 3 --model rf
+python train.py --fold 4 --model rf
+
+# duration
+DURATION=$[ $(date +%s) - ${START} ]
+echo ${DURATION}
