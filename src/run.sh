fa12ad0a82efd9045a7b6ed1c4c8e55b46dec964
Luke Griffiths <griff.luke@gmail.com>
2022-11-06 17:15:41 +0100

    Fixed run script

--- src/run.sh
+++ src/run.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-SECONDS=0
+start=`date +%s`
 
 python train.py --fold 0 --model rf
-#python train.py --fold 1 --model rf
-#python train.py --fold 2 --model rf
-#python train.py --fold 3 --model rf
-#python train.py --fold 4 --model rf
+python train.py --fold 1 --model rf
+python train.py --fold 2 --model rf
+python train.py --fold 3 --model rf
+python train.py --fold 4 --model rf
 
-duration=$SECONDS
+end=`date +%s`
+
+runtime=$((end-start))
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
